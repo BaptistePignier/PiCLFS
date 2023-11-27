559045be4169cae8a54fc909e5507cf14ac1b5a9
bpignier <pignier.baptiste@tutanota.com>
2023-11-27 11:52:36 +0100

    - Remove version number - Add .gitignore

--- 02-build-toolchain.sh
+++ 02-build-toolchain.sh
@@ -48,11 +48,14 @@ function error() {
 }
 
 function extract() {
+    name=$(echo $1 | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1)
+    mkdir $2/$name
+    tar -nxf $1 -C $2/$name --strip-component 1
     case $1 in
-        *.tgz) tar -zxf $1 -C $2 ;;
-        *.tar.gz) tar -zxf $1 -C $2 ;;
-        *.tar.bz2) tar -jxf $1 -C $2 ;;
-        *.tar.xz) tar -Jxf $1 -C $2 ;;
+        *.tgz) tar -zxf $1 -C $2/$name --strip-component 1 ;;
+        *.tar.gz) tar -zxf $1 -C $2/$name --strip-component 1 ;;
+        *.tar.bz2) tar -jxf $1 -C $2/$name --strip-component 1 ;;
+        *.tar.xz) tar -Jxf $1 -C $2/$name --strip-component 1 ;;
     esac
 }
 
@@ -144,16 +147,16 @@ if [[ "$CONFIG_LINUX_ARCH" = "arm64" ]] ; then
     ln -snvf lib $SYSROOT_DIR/lib64
 fi
 
-step "[3/25] Pkgconf 1.6.1"
-extract $SOURCES_DIR/pkgconf-1.6.3.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/pkgconf-1.6.3 && \
+step "[3/25] Pkgconf"
+extract $SOURCES_DIR/pkgconf.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/pkgconf && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared \
     --disable-dependency-tracking )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/pkgconf-1.6.3
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/pkgconf-1.6.3
+make -j$PARALLEL_JOBS -C $BUILD_DIR/pkgconf
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/pkgconf
 cat > $TOOLS_DIR/bin/pkg-config << "EOF"
 #!/bin/sh
 PKGCONFDIR=$(dirname $0)
@@ -171,63 +174,63 @@ EOF
 chmod 755 $TOOLS_DIR/bin/pkg-config
 sed -i -e "s,@STAGING_SUBDIR@,$SYSROOT_DIR,g" $TOOLS_DIR/bin/pkg-config
 sed -i -e "s,@STATIC@,," $TOOLS_DIR/bin/pkg-config
-rm -rf $BUILD_DIR/pkgconf-1.6.3
+rm -rf $BUILD_DIR/pkgconf
 
-step "[4/25] M4 1.4.18"
-extract $SOURCES_DIR/m4-1.4.18.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/m4-1.4.18 && \
+step "[4/25] M4"
+extract $SOURCES_DIR/m4.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/m4 && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/m4-1.4.18
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/m4-1.4.18
-rm -rf $BUILD_DIR/m4-1.4.18
+make -j$PARALLEL_JOBS -C $BUILD_DIR/m4
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/m4
+rm -rf $BUILD_DIR/m4
 
-step "[5/25] Libtool 2.4.6"
-extract $SOURCES_DIR/libtool-2.4.6.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/libtool-2.4.6 && \
+step "[5/25] Libtool"
+extract $SOURCES_DIR/libtool.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/libtool && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/libtool-2.4.6
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/libtool-2.4.6
-rm -rf $BUILD_DIR/libtool-2.4.6
+make -j$PARALLEL_JOBS -C $BUILD_DIR/libtool
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/libtool
+rm -rf $BUILD_DIR/libtool
 
-step "[6/25] Autoconf 2.69"
-extract $SOURCES_DIR/autoconf-2.69.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/autoconf-2.69 && \
+step "[6/25] Autoconf"
+extract $SOURCES_DIR/autoconf.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/autoconf && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/autoconf-2.69
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/autoconf-2.69
-rm -rf $BUILD_DIR/autoconf-2.69
+make -j$PARALLEL_JOBS -C $BUILD_DIR/autoconf
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/autoconf
+rm -rf $BUILD_DIR/autoconf
 
-step "[7/25] Automake 1.16.1"
-extract $SOURCES_DIR/automake-1.16.1.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/automake-1.16.1 && \
+step "[7/25] Automake"
+extract $SOURCES_DIR/automake.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/automake && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/automake-1.16.1
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/automake-1.16.1
+make -j$PARALLEL_JOBS -C $BUILD_DIR/automake
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/automake
 mkdir -p $SYSROOT_DIR/usr/share/aclocal
-rm -rf $BUILD_DIR/automake-1.16.1
+rm -rf $BUILD_DIR/automake
 
-step "[8/25] Zlib 1.2.11"
-extract $SOURCES_DIR/zlib-1.2.11.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/zlib-1.2.11 && ./configure --prefix=$TOOLS_DIR )
-make -j1 -C $BUILD_DIR/zlib-1.2.11
-make -j1 install -C $BUILD_DIR/zlib-1.2.11
-rm -rf $BUILD_DIR/zlib-1.2.11
+step "[8/25] Zlib"
+extract $SOURCES_DIR/zlib.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/zlib && ./configure --prefix=$TOOLS_DIR )
+make -j1 -C $BUILD_DIR/zlib
+make -j1 install -C $BUILD_DIR/zlib
+rm -rf $BUILD_DIR/zlib
 
-step "[9/25] Util-linux 2.35"
-extract $SOURCES_DIR/util-linux-2.35.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/util-linux-2.35 && \
+step "[9/25] Util-linux"
+extract $SOURCES_DIR/util-linux.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/util-linux && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
@@ -266,13 +269,13 @@ extract $SOURCES_DIR/util-linux-2.35.tar.xz $BUILD_DIR
     --disable-wdctl \
     --disable-write \
     --disable-zramctl )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/util-linux-2.35
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/util-linux-2.35
-rm -rf $BUILD_DIR/util-linux-2.35
+make -j$PARALLEL_JOBS -C $BUILD_DIR/util-linux
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/util-linux
+rm -rf $BUILD_DIR/util-linux
 
-step "[10/25] E2fsprogs 1.45.4"
-extract $SOURCES_DIR/e2fsprogs-1.45.4.tar.gz $BUILD_DIR
-( cd $BUILD_DIR/e2fsprogs-1.45.4 && \
+step "[10/25] E2fsprogs"
+extract $SOURCES_DIR/e2fsprogs.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/e2fsprogs && \
     ac_cv_path_LDCONFIG=true \
     ./configure \
     --prefix=$TOOLS_DIR \
@@ -287,53 +290,53 @@ extract $SOURCES_DIR/e2fsprogs-1.45.4.tar.gz $BUILD_DIR
     --enable-symlink-install \
     --enable-elf-shlibs \
     --with-crond-dir=no )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/e2fsprogs-1.45.4
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/e2fsprogs-1.45.4
-rm -rf $BUILD_DIR/e2fsprogs-1.45.4
+make -j$PARALLEL_JOBS -C $BUILD_DIR/e2fsprogs
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/e2fsprogs
+rm -rf $BUILD_DIR/e2fsprogs
 
-step "[11/25] Fakeroot 1.24"
-extract $SOURCES_DIR/fakeroot_1.24.orig.tar.gz $BUILD_DIR
-( cd $BUILD_DIR/fakeroot-1.24 && \
+step "[11/25] Fakeroot"
+extract $SOURCES_DIR/fakeroot.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/fakeroot && \
     ac_cv_header_sys_capability_h=no \
     ac_cv_func_capset=no \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/fakeroot-1.24
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/fakeroot-1.24
-rm -rf $BUILD_DIR/fakeroot-1.24
+make -j$PARALLEL_JOBS -C $BUILD_DIR/fakeroot
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/fakeroot
+rm -rf $BUILD_DIR/fakeroot
 
-step "[12/25] Bison 3.5"
-extract $SOURCES_DIR/bison-3.5.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/bison-3.5 && \
+step "[12/25] Bison"
+extract $SOURCES_DIR/bison.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/bison && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/bison-3.5
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/bison-3.5
-rm -rf $BUILD_DIR/bison-3.5
+make -j$PARALLEL_JOBS -C $BUILD_DIR/bison
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/bison
+rm -rf $BUILD_DIR/bison
 
-step "[13/25] Gawk 5.0.1"
-extract $SOURCES_DIR/gawk-5.0.1.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/gawk-5.0.1 && \
+step "[13/25] Gawk"
+extract $SOURCES_DIR/gawk.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/gawk && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared \
     --without-readline \
     --without-mpfr )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/gawk-5.0.1
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/gawk-5.0.1
-rm -rf $BUILD_DIR/gawk-5.0.1
+make -j$PARALLEL_JOBS -C $BUILD_DIR/gawk
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/gawk
+rm -rf $BUILD_DIR/gawk
 
-step "[14/25] Binutils 2.33.1"
-extract $SOURCES_DIR/binutils-2.33.1.tar.xz $BUILD_DIR
-mkdir -pv $BUILD_DIR/binutils-2.33.1/binutils-build
-( cd $BUILD_DIR/binutils-2.33.1/binutils-build && \
+step "[14/25] Binutils"
+extract $SOURCES_DIR/binutils.tar.xz $BUILD_DIR
+mkdir -pv $BUILD_DIR/binutils/binutils-build
+( cd $BUILD_DIR/binutils/binutils-build && \
     MAKEINFO=true \
-    $BUILD_DIR/binutils-2.33.1/configure \
+    $BUILD_DIR/binutils/configure \
     --prefix=$TOOLS_DIR \
     --target=$CONFIG_TARGET \
     --disable-multilib \
@@ -344,25 +347,25 @@ mkdir -pv $BUILD_DIR/binutils-2.33.1/binutils-build
     --enable-poison-system-directories \
     --disable-sim \
     --disable-gdb )
-make -j$PARALLEL_JOBS configure-host -C $BUILD_DIR/binutils-2.33.1/binutils-build
-make -j$PARALLEL_JOBS -C $BUILD_DIR/binutils-2.33.1/binutils-build
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/binutils-2.33.1/binutils-build
-rm -rf $BUILD_DIR/binutils-2.33.1
+make -j$PARALLEL_JOBS configure-host -C $BUILD_DIR/binutils/binutils-build
+make -j$PARALLEL_JOBS -C $BUILD_DIR/binutils/binutils-build
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/binutils/binutils-build
+rm -rf $BUILD_DIR/binutils
 
-step "[15/25] Gcc 9.2.0 - Static"
-tar -Jxf $SOURCES_DIR/gcc-9.2.0.tar.xz -C $BUILD_DIR
-extract $SOURCES_DIR/gmp-6.1.2.tar.xz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/gmp-6.1.2 $BUILD_DIR/gcc-9.2.0/gmp
-extract $SOURCES_DIR/mpfr-4.0.2.tar.xz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/mpfr-4.0.2 $BUILD_DIR/gcc-9.2.0/mpfr
-extract $SOURCES_DIR/mpc-1.1.0.tar.gz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/mpc-1.1.0 $BUILD_DIR/gcc-9.2.0/mpc
-mkdir -pv $BUILD_DIR/gcc-9.2.0/gcc-build
-( cd $BUILD_DIR/gcc-9.2.0/gcc-build && \
+step "[15/25] Gcc - Static"
+tar -Jxf $SOURCES_DIR/gcc.tar.xz -C $BUILD_DIR
+extract $SOURCES_DIR/gmp.tar.xz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/gmp $BUILD_DIR/gcc/gmp
+extract $SOURCES_DIR/mpfr.tar.xz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/mpfr $BUILD_DIR/gcc/mpfr
+extract $SOURCES_DIR/mpc.tar.gz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/mpc $BUILD_DIR/gcc/mpc
+mkdir -pv $BUILD_DIR/gcc/gcc-build
+( cd $BUILD_DIR/gcc/gcc-build && \
     MAKEINFO=missing \
     CFLAGS_FOR_TARGET="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -Os" \
     CXXFLAGS_FOR_TARGET="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -Os" \
-    $BUILD_DIR/gcc-9.2.0/configure \
+    $BUILD_DIR/gcc/configure \
     --prefix=$TOOLS_DIR \
     --build=$CONFIG_HOST \
     --host=$CONFIG_HOST \
@@ -387,21 +390,21 @@ mkdir -pv $BUILD_DIR/gcc-9.2.0/gcc-build
     --disable-threads \
     --with-newlib \
     --disable-largefile )
-make -j$PARALLEL_JOBS gcc_cv_libc_provides_ssp=yes all-gcc all-target-libgcc -C $BUILD_DIR/gcc-9.2.0/gcc-build
-make -j$PARALLEL_JOBS install-gcc install-target-libgcc -C $BUILD_DIR/gcc-9.2.0/gcc-build
-rm -rf $BUILD_DIR/gcc-9.2.0
+make -j$PARALLEL_JOBS gcc_cv_libc_provides_ssp=yes all-gcc all-target-libgcc -C $BUILD_DIR/gcc/gcc-build
+make -j$PARALLEL_JOBS install-gcc install-target-libgcc -C $BUILD_DIR/gcc/gcc-build
+rm -rf $BUILD_DIR/gcc
 
-step "[16/25] Raspberry Pi Linux 4.19.93 API Headers"
-extract $SOURCES_DIR/raspberrypi-kernel_1.20200114-1.tar.gz $BUILD_DIR
-make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH mrproper -C $BUILD_DIR/linux-raspberrypi-kernel_1.20200114-1
-make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH headers_check -C $BUILD_DIR/linux-raspberrypi-kernel_1.20200114-1
-make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH INSTALL_HDR_PATH=$SYSROOT_DIR headers_install -C $BUILD_DIR/linux-raspberrypi-kernel_1.20200114-1
-rm -rf $BUILD_DIR/linux-raspberrypi-kernel_1.20200114-1
+step "[16/25] Raspberry Pi Linux API Headers"
+extract $SOURCES_DIR/kernel.tar.gz $BUILD_DIR
+make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH mrproper -C $BUILD_DIR/kernel
+make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH headers_check -C $BUILD_DIR/kernel
+make -j$PARALLEL_JOBS ARCH=$CONFIG_LINUX_ARCH INSTALL_HDR_PATH=$SYSROOT_DIR headers_install -C $BUILD_DIR/kernel
+rm -rf $BUILD_DIR/kernel
 
-step "[17/25] glibc 2.30"
-extract $SOURCES_DIR/glibc-2.30.tar.xz $BUILD_DIR
-mkdir $BUILD_DIR/glibc-2.30/glibc-build
-( cd $BUILD_DIR/glibc-2.30/glibc-build && \
+step "[17/25] glibc"
+extract $SOURCES_DIR/glibc.tar.xz $BUILD_DIR
+mkdir $BUILD_DIR/glibc/glibc-build
+( cd $BUILD_DIR/glibc/glibc-build && \
     CC="$TOOLS_DIR/bin/$CONFIG_TARGET-gcc" \
     CXX="$TOOLS_DIR/bin/$CONFIG_TARGET-g++" \
     AR="$TOOLS_DIR/bin/$CONFIG_TARGET-ar" \
@@ -414,7 +417,7 @@ mkdir $BUILD_DIR/glibc-2.30/glibc-build
     ac_cv_path_BASH_SHELL=/bin/sh \
     libc_cv_forced_unwind=yes \
     libc_cv_ssp=no \
-    $BUILD_DIR/glibc-2.30/configure \
+    $BUILD_DIR/glibc/configure \
     --target=$CONFIG_TARGET \
     --host=$CONFIG_TARGET \
     --build=$CONFIG_HOST \
@@ -426,24 +429,24 @@ mkdir $BUILD_DIR/glibc-2.30/glibc-build
     --enable-obsolete-rpc \
     --enable-kernel=4.19 \
     --with-headers=$SYSROOT_DIR/usr/include )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/glibc-2.30/glibc-build
-make -j$PARALLEL_JOBS install_root=$SYSROOT_DIR install -C $BUILD_DIR/glibc-2.30/glibc-build
-rm -rf $BUILD_DIR/glibc-2.30
+make -j$PARALLEL_JOBS -C $BUILD_DIR/glibc/glibc-build
+make -j$PARALLEL_JOBS install_root=$SYSROOT_DIR install -C $BUILD_DIR/glibc/glibc-build
+rm -rf $BUILD_DIR/glibc
 
-step "[18/25] Gcc 9.2.0 - Final"
-tar -Jxf $SOURCES_DIR/gcc-9.2.0.tar.xz -C $BUILD_DIR
-extract $SOURCES_DIR/gmp-6.1.2.tar.xz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/gmp-6.1.2 $BUILD_DIR/gcc-9.2.0/gmp
-extract $SOURCES_DIR/mpfr-4.0.2.tar.xz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/mpfr-4.0.2 $BUILD_DIR/gcc-9.2.0/mpfr
-extract $SOURCES_DIR/mpc-1.1.0.tar.gz $BUILD_DIR/gcc-9.2.0
-mv -v $BUILD_DIR/gcc-9.2.0/mpc-1.1.0 $BUILD_DIR/gcc-9.2.0/mpc
-mkdir -v $BUILD_DIR/gcc-9.2.0/gcc-build
-( cd $BUILD_DIR/gcc-9.2.0/gcc-build && \
+step "[18/25] Gcc - Final"
+tar -Jxf $SOURCES_DIR/gcc.tar.xz -C $BUILD_DIR
+extract $SOURCES_DIR/gmp.tar.xz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/gmp $BUILD_DIR/gcc/gmp
+extract $SOURCES_DIR/mpfr.tar.xz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/mpfr $BUILD_DIR/gcc/mpfr
+extract $SOURCES_DIR/mpc.tar.gz $BUILD_DIR/gcc
+mv -v $BUILD_DIR/gcc/mpc $BUILD_DIR/gcc/mpc
+mkdir -v $BUILD_DIR/gcc/gcc-build
+( cd $BUILD_DIR/gcc/gcc-build && \
     MAKEINFO=missing \
     CFLAGS_FOR_TARGET="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -Os" \
     CXXFLAGS_FOR_TARGET="-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -Os" \
-    $BUILD_DIR/gcc-9.2.0/configure \
+    $BUILD_DIR/gcc/configure \
     --prefix=$TOOLS_DIR \
     --build=$CONFIG_HOST \
     --host=$CONFIG_HOST \
@@ -463,74 +466,74 @@ mkdir -v $BUILD_DIR/gcc-9.2.0/gcc-build
     --with-build-time-tools=$TOOLS_DIR/$CONFIG_TARGET/bin \
     --enable-shared \
     --disable-libgomp )
-make -j$PARALLEL_JOBS gcc_cv_libc_provides_ssp=yes -C $BUILD_DIR/gcc-9.2.0/gcc-build
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/gcc-9.2.0/gcc-build
+make -j$PARALLEL_JOBS gcc_cv_libc_provides_ssp=yes -C $BUILD_DIR/gcc/gcc-build
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/gcc/gcc-build
 if [ ! -e $TOOLS_DIR/bin/$CONFIG_TARGET-cc ]; then
     ln -vf $TOOLS_DIR/bin/$CONFIG_TARGET-gcc $TOOLS_DIR/bin/$CONFIG_TARGET-cc
 fi
-rm -rf $BUILD_DIR/gcc-9.2.0
+rm -rf $BUILD_DIR/gcc
 
-step "[19/25] Elfutils 0.178"
-extract $SOURCES_DIR/elfutils-0.178.tar.bz2 $BUILD_DIR
-( cd $BUILD_DIR/elfutils-0.178 && \
+step "[19/25] Elfutils"
+extract $SOURCES_DIR/elfutils.tar.bz2 $BUILD_DIR
+( cd $BUILD_DIR/elfutils && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared \
     --disable-debuginfod )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/elfutils-0.178
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/elfutils-0.178
-rm -rf $BUILD_DIR/elfutils-0.178
+make -j$PARALLEL_JOBS -C $BUILD_DIR/elfutils
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/elfutils
+rm -rf $BUILD_DIR/elfutils
 
-step "[20/25] Dosfstools 4.1"
-extract $SOURCES_DIR/dosfstools-4.1.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/dosfstools-4.1 && \
+step "[20/25] Dosfstools"
+extract $SOURCES_DIR/dosfstools.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/dosfstools && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared \
     --enable-compat-symlinks )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/dosfstools-4.1
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/dosfstools-4.1
-rm -rf $BUILD_DIR/dosfstools-4.1
+make -j$PARALLEL_JOBS -C $BUILD_DIR/dosfstools
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/dosfstools
+rm -rf $BUILD_DIR/dosfstools
 
-step "[21/25] libconfuse 3.2.2"
-extract $SOURCES_DIR/confuse-3.2.2.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/confuse-3.2.2 && \
+step "[21/25] libconfuse"
+extract $SOURCES_DIR/confuse.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/confuse && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/confuse-3.2.2
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/confuse-3.2.2
-rm -rf $BUILD_DIR/confuse-3.2.2
+make -j$PARALLEL_JOBS -C $BUILD_DIR/confuse
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/confuse
+rm -rf $BUILD_DIR/confuse
 
-step "[22/25] Genimage 11"
-extract $SOURCES_DIR/genimage-11.tar.xz $BUILD_DIR
-( cd $BUILD_DIR/genimage-11 && \
+step "[22/25] Genimage"
+extract $SOURCES_DIR/genimage.tar.xz $BUILD_DIR
+( cd $BUILD_DIR/genimage && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/genimage-11
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/genimage-11
-rm -rf $BUILD_DIR/genimage-11
+make -j$PARALLEL_JOBS -C $BUILD_DIR/genimage
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/genimage
+rm -rf $BUILD_DIR/genimage
 
-step "[23/25] Flex 2.6.4"
-extract $SOURCES_DIR/flex-2.6.3.tar.gz $BUILD_DIR
-( cd $BUILD_DIR/flex-2.6.3 && \
+step "[23/25] Flex"
+extract $SOURCES_DIR/flex.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/flex && \
     ./configure \
     --prefix=$TOOLS_DIR \
     --disable-static \
     --enable-shared \
     --disable-doc )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/flex-2.6.3
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/flex-2.6.3
-rm -rf $BUILD_DIR/flex-2.6.3
+make -j$PARALLEL_JOBS -C $BUILD_DIR/flex
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/flex
+rm -rf $BUILD_DIR/flex
 
-step "[24/25] Mtools 4.0.23"
-extract $SOURCES_DIR/mtools-4.0.23.tar.bz2 $BUILD_DIR
-( cd $BUILD_DIR/mtools-4.0.23 && \
+step "[24/25] Mtools"
+extract $SOURCES_DIR/mtools.tar.bz2 $BUILD_DIR
+( cd $BUILD_DIR/mtools && \
     ac_cv_lib_bsd_gethostbyname=no \
     ac_cv_lib_bsd_main=no \
     ac_cv_path_INSTALL_INFO= \
@@ -539,13 +542,13 @@ extract $SOURCES_DIR/mtools-4.0.23.tar.bz2 $BUILD_DIR
     --disable-static \
     --enable-shared \
     --disable-doc )
-make -j1 -C $BUILD_DIR/mtools-4.0.23
-make -j1 install -C $BUILD_DIR/mtools-4.0.23
-rm -rf $BUILD_DIR/mtools-4.0.23
+make -j1 -C $BUILD_DIR/mtools
+make -j1 install -C $BUILD_DIR/mtools
+rm -rf $BUILD_DIR/mtools
 
-step "[25/25] Openssl 1.1.1d"
-extract $SOURCES_DIR/openssl-1.1.1d.tar.gz $BUILD_DIR
-( cd $BUILD_DIR/openssl-1.1.1d && \
+step "[25/25] Openssl "
+extract $SOURCES_DIR/openssl.tar.gz $BUILD_DIR
+( cd $BUILD_DIR/openssl && \
     ./config \
     --prefix=$TOOLS_DIR \
     --openssldir=$TOOLS_DIR/etc/ssl \
@@ -555,9 +558,9 @@ extract $SOURCES_DIR/openssl-1.1.1d.tar.gz $BUILD_DIR
     no-fuzz-afl \
     shared \
     zlib-dynamic )
-make -j$PARALLEL_JOBS -C $BUILD_DIR/openssl-1.1.1d
-make -j$PARALLEL_JOBS install -C $BUILD_DIR/openssl-1.1.1d
-rm -rf $BUILD_DIR/openssl-1.1.1d
+make -j$PARALLEL_JOBS -C $BUILD_DIR/openssl
+make -j$PARALLEL_JOBS install -C $BUILD_DIR/openssl
+rm -rf $BUILD_DIR/openssl
 
 do_strip
 
